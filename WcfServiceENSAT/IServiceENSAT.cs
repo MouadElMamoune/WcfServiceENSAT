cb2c2445c773b32c235f1ee2c9280354342a3161
HP <HP@DESKTOP-B9GG1G3>
2021-12-20 15:17:02 +0100

    Ajoutez des fichiers projet.

---
+++ WcfServiceENSAT/IServiceENSAT.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace WcfServiceENSAT
+{
+    // REMARQUE : vous pouvez utiliser la commande Renommer du menu Refactoriser pour changer le nom d'interface "IServiceENSAT" à la fois dans le code et le fichier de configuration.
+    [ServiceContract]
+    public interface IServiceENSAT
+    {
+        [OperationContract]
+        Student getStudent(int id);
+
+        [OperationContract]
+        void addStudent(Student s);
+
+        [OperationContract]
+        void deleteStudent(int id);
+
+        [OperationContract]
+        void updateStudent(Student s);
+
+        [OperationContract]
+        List<Student> getAllStudents();
+    }
+}
---
+++ WcfServiceENSAT/ServiceENSAT.svc.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace WcfServiceENSAT
+{
+    // REMARQUE : vous pouvez utiliser la commande Renommer du menu Refactoriser pour changer le nom de classe "ServiceENSAT" à la fois dans le code, le fichier svc et le fichier de configuration.
+    // REMARQUE : pour lancer le client test WCF afin de tester ce service, sélectionnez ServiceENSAT.svc ou ServiceENSAT.svc.cs dans l'Explorateur de solutions et démarrez le débogage.
+    public class ServiceENSAT : IServiceENSAT
+    {
+        DataClassesENSATDataContext d = new DataClassesENSATDataContext();
+        public Student getStudent(int id)
+        {
+            return (from p in d.Student where p.ID == id select p).FirstOrDefault();
+        }
+
+        public void addStudent(Student s)
+        {
+            d.Student.InsertOnSubmit(s);
+            d.SubmitChanges();
+        }
+
+        public void deleteStudent(int id)
+        {
+            d.Student.DeleteOnSubmit(this.getStudent(id));
+            d.SubmitChanges();
+        }
+
+        public void updateStudent(Student s)
+        {
+            Student stud = new Student();
+            stud.ID = s.ID ;
+            stud.FNAME = s.FNAME;
+            stud.LNAME = s.LNAME;
+            stud.MAIL = s.MAIL;
+            stud.PHONE = s.PHONE;
+            d.Student.DeleteOnSubmit(this.getStudent(s.ID));
+            d.Student.InsertOnSubmit(stud);
+            d.SubmitChanges();
+        }
+
+        public List<Student> getAllStudents()
+        {
+            return d.Student.ToList();
+        }
+    }
+}
